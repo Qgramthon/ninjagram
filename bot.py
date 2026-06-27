24126c84c1a2415fa6d60386ee650c618d15db4f
Ninjagram <tahounxgaming@gmail.com>
2026-06-27 19:50:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,6 @@ async def bot_start(event):
         [Button.url("بدء التنصيب", "https://t.me/nj_rm_bot?profile")]
     ]
     caption = (
-        "**• إضغط بدء التنصيب ثم فتح التطبيق 🜲**"
     )
 
     if os.path.exists(START_IMAGE):
